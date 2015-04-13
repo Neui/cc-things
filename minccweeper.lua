fee7e8c281f45f2f26ff7b1a6855d894c8957aa5
Neui <neuisen@googlemail.com>
2015-04-13 16:04:49 +0200

    minccweeper: basic menu with selection

--- minccweeper.lua
+++ minccweeper.lua
@@ -17,6 +17,10 @@ f.draw = {} -- Drawing functions
 
 local v = {} -- Variables
 v.state = 1 -- State 0 = startup, 1 = Main menu, 2 = Ingame
+v.sel = {}
+v.sel.title = 1
+v.sel.customField = 0
+v.titleSelection = {}
 v.field = {}
 v.field.width = 9
 v.field.height = 9
@@ -27,43 +31,23 @@ v.draw.fieldMoved = true
 v.draw.redraw = true
 
 local c = {} -- constants
+c.name = "minCCweeper"
+c.title = "~[ " .. c.name .. " ]~"
 c.field = {}
 c.field.maxwidth = 30  -- Taken from windows minesweeper lol
 c.field.maxheight = 24
 c.field.minmines = 10
 c.field.maxmines = 668
 c.field.draw = {}
-c.field.draw.unknown = {}
-c.field.draw.unknown.char = '#'
-c.field.draw.unknown.fgmono = colors.white
-c.field.draw.unknown.bgadv = colors.gray
-c.field.draw.unknown.bgmono = colors.black
-c.field.draw.nothing = {}
-c.field.draw.nothing.char = ' '
-c.field.draw.mine = {}
-c.field.draw.mine.char = 'O'
-c.field.draw.mine.fgadv = colors.red
-c.field.draw.mightMine = {}
-c.field.draw.mightMine.char = 'P'
-c.field.draw.mightMine.fgadv = colors.lightGray
-c.field.draw.numbers = {}
-c.field.draw.numbers.fg = { colors.blue, colors.green, colors.red, colors.blue, colors.brown, colors.lightBlue, colors.gray, colors.lightGray }
+c.field.draw.unknown = { ["char"] = '#'; ["fgmono"] = colors.white; ["bgadv"] = colors.gray; ["bgmono"] = colors.black; }
+c.field.draw.nothing = { ["char"] = ' '; }
+c.field.draw.mine = { ["char"] = 'O'; ["fgadv"] = colors.red; }
+c.field.draw.mightMine = { ["char"] = 'P'; ["fgadv"] = colors.lightGray; }
+c.field.draw.numbers = { ["fg"] = { colors.blue, colors.green, colors.red, colors.blue, colors.brown, colors.lightBlue, colors.gray, colors.lightGray }; }
 c.field.presets = {} -- Also taken form windows minesweeper
-c.field.presets.beginner = {}
-c.field.presets.beginner.name = "Beginner"
-c.field.presets.beginner.width = 9
-c.field.presets.beginner.height = 9
-c.field.presets.beginner.mines = 10
-c.field.presets.advanced = {}
-c.field.presets.advanced.name = "Advanced"
-c.field.presets.advanced.width = 16
-c.field.presets.advanced.height = 16
-c.field.presets.advanced.mines = 40
-c.field.presets.pro = {}
-c.field.presets.pro.name = "Professional"
-c.field.presets.pro.width = 30
-c.field.presets.pro.height = 16
-c.field.presets.pro.mines = 99
+c.field.presets.beginner = { ["name"] = "Beginner"; ["width"] =  9; ["height"] =  9; ["mines"] = 10; }
+c.field.presets.advanced = { ["name"] = "Advanced"; ["width"] = 16; ["height"] = 16; ["mines"] = 40; }
+c.field.presets.pro =  { ["name"] = "Professional"; ["width"] = 30; ["height"] = 16; ["mines"] = 99; }
 
 local playfield = {} -- Contains all of the bombs etc.
 local playerfiled = {} -- What the players sees
@@ -79,10 +63,10 @@ end
 function f.drawLoop()
   while running do
     if v.draw.redraw then
+      f.draw.setTBGColor(term, colors.white, colors.black)
       term.clear()
     end
     if v.state == 1 then -- Main Selection (DL|RUN)
-      term.clear()
       f.draw.title(term, w, h)
       f.draw.titleSelection(term, w, h)
       f.draw.fieldSizeAndWidth(term, w, h)
@@ -115,11 +99,20 @@ function f.draw.footer(term, w, h)
 end
 
 function f.draw.title(term, w, h)
-
+  term.setCursorPos((w/2)-(#c.title/2), h/#v.titleSelection)
+  term.write(c.title)
 end
 
 function f.draw.titleSelection(term, w, h)
-
+  for k, val in ipairs(v.titleSelection) do
+    term.setCursorPos((w/2)-(#val.name/2), (h/2)-(#v.titleSelection/2)+k)
+    if k == v.sel.title then
+      f.draw.setTBGColor(term, colors.black, colors.lightGray, colors.white)
+    else
+      f.draw.setTBGColor(term, colors.white, colors.black)  
+    end
+    term.write(val.name)
+  end
 end
 
 function f.draw.fieldSizeAndWidth(term, w, h)
@@ -160,6 +153,12 @@ function f.inputLoop()
       running = false
     elseif ev == "term.resize" then
       w, h = term.getSize()
+    elseif ev == "key" then
+      if p1 == 200 then -- Up Arrow
+        v.sel.title = ((v.sel.title - 2) % #v.titleSelection) + 1
+      elseif p1 == 208 then -- Down Arrow
+        v.sel.title = (v.sel.title % #v.titleSelection) + 1
+      end
     end
   end
 end
@@ -173,7 +172,15 @@ end
 local function init()
   local notice = false
   f.draw.setTextColor(term, colors.white)
-  -- Nothing yet
+  
+  -- Setup the titlescreen selection
+  for k, val in pairs(c.field.presets) do
+    v.titleSelection[#v.titleSelection + 1] = val
+  end
+  v.titleSelection[#v.titleSelection + 1] = {["name"] = "Custom"; ["function"] = "custom";}
+  v.titleSelection[#v.titleSelection + 1] = {["name"] = "Exit"; ["function"] = "exit"; }
+  
+  
   if notice and running then
     write("\nPress a key to continue... (or wait 10s)")
     local tim, ev, p1 = os.startTimer(10)
